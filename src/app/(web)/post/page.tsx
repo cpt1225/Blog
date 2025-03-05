4134de2fc7f6d6d07549f8f4e9cc3c6d8b78db94
cpt1225 <2217013491@qq.com>
2025-03-05 10:41:24 +0800

    bug:修改部分代码重写response带来的bug

--- src/app/(web)/post/page.tsx
+++ src/app/(web)/post/page.tsx
@@ -3,24 +3,23 @@ import { Post } from '@/types'
 import { useEffect, useState } from "react"
 import request from '@/lib/axios'
 import BlogCard from '@/components/Card/BlogCard'
+import { toast,ToastContainer } from 'react-toastify'
 
 
 const Page = () => {
   const [isloading, setIsLoading] = useState(false)
   const [posts, setPosts] = useState<Post[]>([])
-  const [error, setError] = useState('')
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true)
       try {
         const res = await request({ url: '/post' })
-        if (res.status !== 200) {
-          throw new Error('未获取到博客')
+        if (res.data.status !== 200) {
+          toast.error(res.data.message)
         }
-        setPosts(res.data || [])
-      } catch (error) {
-        console.log('error', error)
-        setError('网络错误')
+        setPosts(res.data.data || [])
+      } catch  {
+        toast.error('网络错误')
       } finally {
         setIsLoading(false)
       }
@@ -31,6 +30,7 @@ const Page = () => {
     <>
       <div className=' mx-10 mt-10 md:mx-20'>
         <h1 className='text-2xl text-nowrap'>最新文章</h1>
+        <ToastContainer />
         {isloading ? (
           <div>加载中...</div>
         ) : (
@@ -42,7 +42,7 @@ const Page = () => {
                 )}
               </div>
             ) : (
-              <div>{error || 'hello'}</div>
+              <div>系统故障</div>
             )
         )}
       </div>
