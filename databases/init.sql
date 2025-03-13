398840338d0455d1f023ae21ef69ba28bb179810
cpt1225 <2217013491@qq.com>
2025-03-13 19:00:15 +0800

    bug: 修改init.sql

--- databases/init.sql
+++ databases/init.sql
@@ -1,26 +1,27 @@
+de
 create database if not exists blog;
 use blog;
 
-CREATE TABLE user (
-  id int NOT NULL AUTO_INCREMENT,
-  avatar varchar(191)  NOT NULL DEFAULT 'default.png',
-  username varchar(191)   DEFAULT NULL,
-  email varchar(191)   NOT NULL,
-  create_time datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  password varchar(191)  NOT NULL,
-  role varchar(191)  DEFAULT 'user',
-  PRIMARY KEY (id),
-  UNIQUE KEY  (email),
-  UNIQUE KEY  (username)
-) AUTO_INCREMENT= 0;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `avatar` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'default.png',
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `User_email_key` (`email`),
+  UNIQUE KEY `User_username_key` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
-CREATE TABLE post (
-  id int NOT NULL AUTO_INCREMENT,
-  title varchar(191)  NOT NULL,
-  content longtext ,
-  create_time datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  author_id int NOT NULL,
-  PRIMARY KEY (id),
-  KEY Post_author_id_fkey (author_id),
-  CONSTRAINT Post_author_id_fkey FOREIGN KEY (author_id) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE CASCADE
-)  AUTO_INCREMENT=10000;
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8mb4_unicode_ci,
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `author_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Post_author_id_fkey` (`author_id`),
+  CONSTRAINT `Post_author_id_fkey` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
